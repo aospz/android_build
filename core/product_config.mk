52c4757485e0f9888de6ee1652bef51abe33605c
joohyoung Yang <yangjoo@debian.yangjoo.net>
2016-04-29 10:00:43 +0900

    add aokp product config
    Change-Id: Ic6e5f98a02fd74efa059b07cc45c0b163cbb2978

--- core/product_config.mk
+++ core/product_config.mk
@@ -185,11 +185,16 @@ ifneq ($(strip $(TARGET_BUILD_APPS)),)
 all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
-# Read in all of the product definitions specified by the AndroidProducts.mk
-# files in the tree.
-all_product_configs := $(get-all-product-makefiles)
+  ifneq ($(AOKP_PRODUCT),)
+    all_product_configs := $(shell ls vendor/aokp/products/${AOKP_PRODUCT}.mk)
+  else
+    # Read in all of the product definitions specified by the AndroidProducts.mk
+    # files in the tree.
+    all_product_configs := $(get-all-product-makefiles)
+  endif # AOKP_PRODUCT
 endif
 
+ifeq ($(AOKP_PRODUCT),)
 # Find the product config makefile for the current product.
 # all_product_configs consists items like:
 # <product_name>:<path_to_the_product_makefile>
@@ -208,9 +213,14 @@ $(foreach f, $(all_product_configs),\
         $(eval all_product_makefiles += $(f))\
         $(if $(filter $(TARGET_PRODUCT),$(basename $(notdir $(f)))),\
             $(eval current_product_makefile += $(f)),)))
+
 _cpm_words :=
 _cpm_word1 :=
 _cpm_word2 :=
+else
+    current_product_makefile := $(strip $(all_product_configs))
+    all_product_makefiles := $(strip $(all_product_configs))
+endif
 current_product_makefile := $(strip $(current_product_makefile))
 all_product_makefiles := $(strip $(all_product_makefiles))
 
